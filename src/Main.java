c59eefb3e8db9af0b7573cf4fe6e8264aa23b5da
olesy <olesyagalieva97@gmail.com>
2022-11-05 02:01:39 +0500

    ДЗ 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        System.out.println(" Задание 1. ");
+        int clientOS = 1;
+        if(clientOS==1 ){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0){
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        // Задание 2
+        System.out.println(" Задание 2. ");
+        int clientDeviceYear = 2014;
+        if ( clientDeviceYear>= 2015 ){
+            System.out.println(" Установите версию приложения для iOS по ссылке" );
+        } else {
+            System.out.println( "Установите облегченную версию приложения для iOS по ссылке ");
+        }
+        if (clientDeviceYear>= 2015+ clientOS){
+            System.out.println(" Установите версию приложения для Android по ссылке" );
+        } else {
+            System.out.println( "Установите облегченную версию приложения для Android по ссылке ");
+        }
+        // Задание 3
+        System.out.println( "Задание 3.");
+
+
+
+
+
     }
 }
