1b1600942f808e095f697902b16be7c06fac5f34
olesy <olesyagalieva97@gmail.com>
2022-11-05 02:40:39 +0500

    ДЗ 5

--- src/Main.java
+++ src/Main.java
@@ -21,8 +21,36 @@ public class Main {
         } else {
             System.out.println( "Установите облегченную версию приложения для Android по ссылке ");
         }
-        // Задание 3
-        System.out.println( "Задание 3.");
+        // Задание 5
+        System.out.println( "Задание 5.");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+                System.out.println( " Зима ");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println(" Весна ");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println(" Лето ");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println(" Осень ");
+                break;
+            case 12:
+                System.out.println(" Зима ");
+                break;
+            default:
+                System.out.println(" Такого месяца не существует ");
+
+        }
 
 
 
